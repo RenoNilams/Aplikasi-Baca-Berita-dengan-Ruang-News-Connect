da92e8b96d9a9b8b1b33528813479b57957cf614
Reno <renonilamsari2806@gmail.com>
2024-12-21 23:25:18 +0700

    Init commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+
 // Set view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
